961fc929bc134d57105a8344a325133e4afb82ce
Julio Labarca <jlabarca25@gmail.com>
2021-08-18 21:11:12 +0100

    basic nconf

--- bot.js
+++ bot.js
@@ -1,5 +1,29 @@
 import orionx from 'orionx-sdk'
 import cron from 'cron'
+import nconf from 'nconf'
+import winston from 'winston'
+
+nconf.argv()
+   .env()
+   .file({
+		file: 'config.json'
+	});
+
+const log = winston.createLogger({
+  level: nconf.get('log_level'),
+  format: winston.format.combine(
+	winston.format.colorize(),
+	winston.format.splat(),
+	winston.format.timestamp(),
+	winston.format.printf(info => `${info.timestamp.replace('T', ' ').replace('Z', '')}[${info.level}]: ${info.message };`)
+  ),
+  defaultMeta: { service: 'orionx-dca-bot' },
+  transports: [
+	new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 
 export default {
 	orionx,
@@ -11,10 +35,11 @@ export default {
 		  })		  
 	},
 	cronSchedule(cronTime) {
+		log.debug('Initializing cron schedule');
 		var CronJob = cron.CronJob;
 		var job = new CronJob(cronTime, function() {
-			console.log('You will see this message every second');
-		}, null, true, 'America/Los_Angeles');
+			log.info('You will see this message every second');
+		});
 		job.start();
 	}
 }
