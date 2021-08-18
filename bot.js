6df2607d523395567dad5d2dcd7f07170ba400d9
Julio Labarca <jlabarca25@gmail.com>
2021-08-18 15:11:53 +0100

    logger config

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ import orionx from 'orionx-sdk'
 import cron from 'cron'
 import nconf from 'nconf'
 import winston from 'winston'
+import moment from 'moment'
+import 'winston-daily-rotate-file';
 
 nconf.argv()
    .env()
@@ -9,19 +11,33 @@ nconf.argv()
 		file: 'config.json'
 	});
 
+const appName = 'dca-bot';
+const dateFormat = 'DD-MM-YYYY HH:mm:ss';
+const logLevel = nconf.get('log_level');
+
 const log = winston.createLogger({
-  level: nconf.get('log_level'),
+  level: logLevel,
+  exitOnError: false,
   format: winston.format.combine(
 	winston.format.colorize(),
-	winston.format.splat(),
-	winston.format.timestamp(),
-	winston.format.printf(info => `${info.timestamp.replace('T', ' ').replace('Z', '')}[${info.level}]: ${info.message };`)
+	winston.format.printf(info => `${moment(info.timestamp).format(dateFormat)} [${info.level}]: ${info.message }`)
   ),
-  defaultMeta: { service: 'orionx-dca-bot' },
+  defaultMeta: { service: appName },
   transports: [
-	new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+	new(winston.transports.DailyRotateFile)({
+		filename: appName,
+		dirname: './logs',
+		datePattern: 'DD-MMM-YYYY',
+		timestamp: moment().format,
+		maxSize: '20m',
+    	maxFiles: '7d',
+		level: logLevel,
+      	format: winston.format.combine(winston.format.uncolorize())
+	}),
+	new(winston.transports.Console)({
+		colorize: true,
+		timestamp: moment().format
+	})
   ],
 });
 
@@ -35,7 +51,7 @@ export default {
 		  })		  
 	},
 	cronSchedule(cronTime) {
-		log.debug('Initializing cron schedule');
+		log.debug('Initializing cron scheduled buy');
 		var CronJob = cron.CronJob;
 		var job = new CronJob(cronTime, function() {
 			log.info('You will see this message every second');
